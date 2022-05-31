b2f8786957a73398572af0e561e8fdb82464bafb
piyush.shitole <74531950+piyushbig@users.noreply.github.com>
2022-05-31 23:48:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { Component } from "react";
+import Plan from "./Plan";
+
+class App extends Component {
+  state = {
+    items: [],
+    text: "",
+  };
+
+  handleChange = (e) => {
+    this.setState({ text: e.target.value });
+  };
+
+  handleAdd = (e) => {
+    if (this.state.text !== "") {
+      const items = [...this.state.items, this.state.text];
+      this.setState({ items: items, text: "" });
+    }
+  };
+
+  handleDelete = (id) => {
+    console.log("deleted", id);
+    const olditems = [...this.state.items];
+    console.log("olditems", olditems);
+    const items = olditems.filter((element, i) => {
+      return i !== id;
+    });
+    this.setState({ items: items });
+  };
+
+  render() {
+    return (
+      <div className="container-fluid my-5">
+        <div className="row">
+          <div className="col-sm-6 mx-auto text-white shadow-lg p-3">
+            <h1 className="text-center">todo list</h1>
+            <div className="row">
+              <div className="col-9">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Write your plan"
+                  value={this.state.text}
+                  onChange={this.handleChange}
+                ></input>
+              </div>
+              <div className="col-2">
+                <button
+                  className="btn btn-warning px-5 font-weight-bold"
+                  onClick={this.handleAdd}
+                >
+                  Add
+                </button>
+              </div>
+              <div className="conatiner-fluid">
+                <ul className="list-unstyles row m-5">
+                  {this.state.items.map((value, i) => {
+                    return (
+                      <Plan
+                        key={i}
+                        id={i}
+                        value={value}
+                        sendData={this.handleDelete}
+                      />
+                    );
+                  })}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
